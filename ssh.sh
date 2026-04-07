27502605152e6b7d16d49a7c16302244d0cddfff
Aaron Montgomery <ammontgo@ncsu.edu>
2026-04-07 23:28:44 +0000

    added training and validation sets

--- ssh.sh
+++ ssh.sh
@@ -1,4 +1,5 @@
-mv ~/id_rsa_openssh ~/.ssh/id_rsa
+chmod 700 ~/.ssh
+cp ~/id_rsa_openssh ~/.ssh/id_rsa
 chmod 400 ~/.ssh/id_rsa
 cd ~/
 git clone git@github.com:ncsu-neural-networks/ece492-final-project.git
